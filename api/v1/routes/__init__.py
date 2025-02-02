e02e94223b801f009879e52339ce935666c1df43
Magnus <nharnharyhaw984@gmail.com>
2025-02-02 21:43:51 +0000

    feat: add habits and habit_logs routes to API router

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,6 +1,8 @@
-from api.v1.routes import users, auth
+from api.v1.routes import users, auth, habits, habit_logs
 from fastapi import APIRouter
 
 api_router = APIRouter()
-api_router.include_router(users.router, prefix="/user", tags=["users"])
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(users.router, prefix="/user", tags=["Users"])
+api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
+api_router.include_router(habits.router, prefix="/habit", tags=["Habit"])
+api_router.include_router(habit_logs.router, prefix="/habit", tags=["Habit Log"])
